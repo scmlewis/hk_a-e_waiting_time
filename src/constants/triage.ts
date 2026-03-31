3db9b6d8b61e720324d69017cb6feca939d4eca4
scmlewis <scmlewis@gmail.com>
2026-03-31 20:36:24 +0800

    chore: clear gh-pages branch for fresh deployment

--- src/constants/triage.ts
+++
@@ -1,7 +0,0 @@
-import type { TriageCategory } from '../types/ae'
-
-export const TRIAGE_KEYS: TriageCategory[] = ['I', 'II', 'III', 'IV_V']
-
-export function getTriageCategoryShortLabel(category: TriageCategory): string {
-  return category === 'IV_V' ? 'IV & V' : category
-}
